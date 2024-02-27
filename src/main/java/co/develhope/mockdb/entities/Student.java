a3e5eacbb72861e8c0c161c279a61a76d5cdd73f
Roccaldo <roccaldodigrisolo@gmail.com>
2024-02-27 18:38:23 +0100

    esx added

---
+++ src/main/java/co/develhope/mockdb/entities/Student.java
@@ -0,0 +1,28 @@
+package co.develhope.mockdb.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+}
